86846ad52015874c554e024fbd8e88105c1e2fb0
har <istarcoming@163.com>
2019-10-27 16:53:50 +0800

    first commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.module.scss'
+
+export const Header = () => {
+    return (
+        <div className={ styles.header }>
+            Header
+        </div>
+    )
+}
